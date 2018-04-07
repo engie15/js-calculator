fb6e2101e47f9be3353157b45fc7888b3d1941e4
engie15 <e.matene15@gmail.com>
2018-04-08 10:08:17 +1200

    added calculator css/html and js files

--- calculator.js
+++ calculator.js
@@ -1,28 +1,56 @@
 //Create required general var 's for calculator
 //(e.g. var entries; var total; var temp)
+var entries = [];
+var total = 0;
+var temp  = '';
+("button").on('click', function () {
+    var val = (this).text();
+    if (!isNaN(val) || val === '.') {
+        temp += val;
+        ("#answer").val(temp.substring(0,10));
+    } else if (val === 'AC') {
+        entries = [];
+        temp = '';
+        total = 0;
+        ("#answer").val('')
+    } else if (val === 'CE'){
+        temp = '';
+        ("#answer").val('')
+    } else if (val === 'X') {
+        entries.push(temp);
+        entries.push('*');
+        temp = '';
+    } else if (val === '/') {
+        entries.push(temp);
+        entries.push('/');
+        temp = '';
+    } else if (val === '=') {
+        entries.push(temp);
 
-var entries = [] //an empty array to represent a part of the numerical expression yet to be calculated.
-var temp = ''  //an empty string.  This will hold a partially entered number.
-var result = 0 //this is where the calculation will be stored when equals
+        var nt = Number(entries[0]);
+        for (var i = 1; i < entries.length; i++) {
+            var nextNum = Number(entries[i+1])
+            var symbol = entries[i];
 
-document.addEventListener('DOMContentLoaded', start)
+        if (symbol === '+') { nt += nextNum; }
+        else if (symbol === '-') { nt -= nexNum; }
+        else if (symbol === '*') { nt *= nextNum; }
+        else if (symbol === '/') { nt /= nextNum; }
 
-function start () {
-    buttonEventListeners('number', processNumber)
-    buttonEventListeners('control', processCtrl)
-    buttonEventListeners('operator', processOperator)
-}
-
-function buttonEventListeners(buttonClass, processFunction) {
-    var buttons = document.getElementsByClassName(buttonClass)
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click',processFunction)
+        i++;
+        }
+    if (nt < 0) {
+        nt = Math.abs(nt) + '-';
     }
-}
-
-function processNumber (evt) {
-    
-}
+    ("#answer").val(nt);
+    entries = [];
+    temp = '';
+    } else {
+        entries.push(temp);
+        entries.push(val);
+        temp = '';
+    }
+});
 
 //Numbers add to temp
 
